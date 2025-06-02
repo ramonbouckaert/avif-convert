ce9bd413ad7b3ddc837bcb4a6c9361907aa09db1
Ramon Bouckaert <ramon@bouckaert.com.au>
2025-06-02 21:42:07 +1000

    Create CMakePresets.json and custom triplets to sidestep problems in ARM64 builds

---
+++ vcpkg-triplets/arm64-linux-dynamic.cmake
@@ -0,0 +1,9 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+# These flags need to be set until the following issue is resolved: https://github.com/microsoft/vcpkg/issues/44260
+set(VCPKG_C_FLAGS "-DLIBYUV_DISABLE_NEON")
+set(VCPKG_CXX_FLAGS "-DLIBYUV_DISABLE_NEON")
